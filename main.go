668aa1111d20610e571a6e86132b45b8b18875f0
Kaan Karakaya <yusufkaan142@gmail.com>
2022-10-28 16:05:37 +0300

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	log.Println("Is still active? nope ita isn't.")
+	log.Println("Is still active? nope ita disn't.")
 	http.HandleFunc("/", HelloServer)
 	_ = http.ListenAndServe(":8080", nil)
 }
